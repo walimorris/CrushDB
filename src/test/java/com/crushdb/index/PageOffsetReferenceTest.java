b9ebfdae32d05dd7f0355f19f456ce5c89d15812
Wali Morris <walimmorris@gmail.com>
2025-03-06 21:06:19 -0800

    add PageOffsetReference tests

---
+++ src/test/java/com/crushdb/index/PageOffsetReferenceTest.java
@@ -0,0 +1,20 @@
+package com.crushdb.index;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PageOffsetReferenceTest {
+
+    @Test
+    void getPageId() {
+        PageOffsetReference pageOffsetReference = new PageOffsetReference(1L, 34);
+        assertEquals(1L, pageOffsetReference.getPageId());
+    }
+
+    @Test
+    void getOffset() {
+        PageOffsetReference pageOffsetReference = new PageOffsetReference(2L, 45);
+        assertEquals(45, pageOffsetReference.getOffset());
+    }
+}
