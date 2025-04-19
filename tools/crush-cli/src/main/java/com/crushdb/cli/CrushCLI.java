5a367236f1f10faba4dd727bc59cac65f5fbe6a8
Wali Morris <walimmorris@gmail.com>
2025-04-18 23:07:53 -0700

    add insert and find to crushcli

--- tools/crush-cli/src/main/java/com/crushdb/cli/CrushCLI.java
+++ tools/crush-cli/src/main/java/com/crushdb/cli/CrushCLI.java
@@ -3,9 +3,12 @@ package com.crushdb.cli;
 import com.crushdb.DatabaseInitializer;
 import com.crushdb.index.btree.SortOrder;
 import com.crushdb.model.document.BsonType;
+import com.crushdb.model.document.Document;
 import com.crushdb.queryengine.QueryEngine;
 import com.crushdb.storageengine.StorageEngine;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class CrushCLI {
@@ -51,7 +54,18 @@ public class CrushCLI {
     }
 
     private void handleInput(String input) {
-        String[] parts = input.split(" ");
+        String[] parts;
+        if (input.contains("insertOne") || input.contains("insertone")) {
+            parts = input.split(" ", 3);
+        } else {
+            parts = input.split(" ");
+        }
+
+        if (input.contains("find")) {
+            parts = input.split(" ", 3);
+        } else {
+            parts = input.split(" ");
+        }
 
         if (parts.length < 2) {
             throw new IllegalArgumentException("Invalid command. At least <command> <crate> required.");
@@ -68,9 +82,25 @@ public class CrushCLI {
             }
             case "createindex" -> {
                 if (parts.length < 7) {
-                    throw new IllegalArgumentException("Invalid createIndex format. Use: createIndex <crate> <bsonType> <indexName> <fieldName> <unique> <order>");
+                    throw new IllegalArgumentException(
+                            "Invalid createIndex format. Use: createIndex <crate> <bsonType> <indexName> <fieldName> <unique> <order>"
+                    );
                 }
                 handleCreateIndex(parts[1], parts[2], parts[3], parts[4], parts[5], parts[6]);
+            }
+            case "insertone" -> {
+                if (parts.length == 3) {
+                    String crateName = parts[1];
+                    String documentJson = parts[2].trim();
+                    handleInsertOne(crateName, documentJson);
+                }
+            }
+            case "find" -> {
+                if (parts.length == 3) {
+                    String crateName = parts[1];
+                    String query = parts[2].trim();
+                    handleFind(crateName, query);
+                }
             }
             default -> throw new IllegalArgumentException("Unknown command: " + command);
         }
@@ -92,6 +122,63 @@ public class CrushCLI {
 
         queryEngine.getCrateManager().getCrate(crateName).createIndex(bsonType, indexName, fieldName, unique, order, sortOrder);
         System.out.println("Created index: " + indexName + " on crate: " + crateName);
+    }
+
+    private void handleInsertOne(String crateName, String json) {
+        System.out.println(json);
+        Document document = Document.fromJson(json);
+        document.prettyPrint();
+        Document resultDocument = queryEngine.getCrateManager().getCrate(crateName).insert(document);
+        resultDocument.prettyPrint();
+    }
+
+    private void handleFind(String crateName, String json) {
+        // {"vehicle_year": { "$gt": 2015 }}
+        String content = json.substring(1);
+        content = content.substring(0, content.length() - 1);
+
+        // we get from example: "vehicle": { "$gt": 2015}
+        // now we work on each part
+        String[] parts = content.split(":", 2);
+        String field = parts[0].trim().replace("\"", "");
+
+        // now we work on operator and value
+        String opAndValuePart = parts[1].trim();
+        opAndValuePart = opAndValuePart.substring(1);
+        opAndValuePart = opAndValuePart.substring(0, opAndValuePart.length() - 1).trim();
+
+        // "$gt": 2015
+        String[] splitsOpAndValue = opAndValuePart.split(":");
+        String operator = splitsOpAndValue[0].trim().replace("\"", "");
+        String value = splitsOpAndValue[1].trim();
+
+        Object typedValue;
+
+        if (value.equalsIgnoreCase("true") || value.equalsIgnoreCase("false")) {
+            typedValue = Boolean.parseBoolean(value);
+        } else if (value.startsWith("\"") && value.endsWith("\"")) {
+            // remove quotes for strings
+            typedValue = value.substring(1, value.length() - 1);
+        } else if (value.contains(".")) {
+            // float or double
+            if (value.length() < 8) {
+                typedValue = Float.parseFloat(value);
+            } else {
+                typedValue = Double.parseDouble(value);
+            }
+        } else {
+            // int or long
+            if (value.length() >= 10) {
+                typedValue = Long.parseLong(value);
+            } else {
+                typedValue = Integer.parseInt(value);
+            }
+        }
+        Map<String, Object> query = Map.of(field, Map.of(operator, typedValue));
+        List<Document> results = queryEngine.find(crateName, query);
+        for (Document result : results) {
+            result.prettyPrint();
+        }
     }
 
     public static void main(String[] args) {
